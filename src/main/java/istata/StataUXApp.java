88b2ddd3c7fe97d675834acd24d7d518131f4296
Markus Schaffner <github@802.ch>
2016-01-19 15:02:40 +0100

    change main app name for convienence

--- src/main/java/istata/StataUXApp.java
+++ src/main/java/istata/StataUXApp.java
@@ -19,9 +19,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class StataUXApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(StataUXApp.class, args);
     }
 }
